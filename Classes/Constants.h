1299928e0a902fb0cb52c6dcada2d27931fabd87
Takeshi Ohishi <skyfive.508@gmail.com>
2010-04-24 10:58:30 -0700

    Add Constants file

---
+++ Classes/Constants.h
@@ -0,0 +1,12 @@
+//
+//  Constants.h
+//  EvernoteTagEditor
+//
+//  Created by Takeshi Ohishi on 4/24/10.
+//  Copyright 2010 DeNA Global, Inc. All rights reserved.
+//
+
+extern const NSString* kUserStoreURLBase;
+extern const NSString* kNoteStoreURLBase;
+extern const NSString* kAPIKey;
+extern const NSString* kAPISecrect;
---
+++ Classes/Constants.m
@@ -0,0 +1,14 @@
+//
+//  Constants.m
+//  EvernoteTagEditor
+//
+//  Created by Takeshi Ohishi on 4/24/10.
+//  Copyright 2010 DeNA Global, Inc. All rights reserved.
+//
+
+#import "Constants.h"
+
+const NSString* kUserStoreURLBase = @"https://sandbox.evernote.com/edam/user";
+const NSString* kNoteStoreURLBase = @"http://sandbox.evernote.com/edam/note/";
+const NSString* kAPIKey = @"starsky5";
+const NSString* kAPISecrect = @"85c680de3776cf88";
